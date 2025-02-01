b3bd9c4d076145259abdd7d7d247b0db974c4f8a
MuhammadHananAsghar <muhammadhananasghar@gmail.com>
2025-02-01 10:41:57 +0500

    Update

--- run_streamlit.py
+++ run_streamlit.py
@@ -2,5 +2,5 @@ import streamlit.web.cli as stcli
 import sys
 
 if __name__ == "__main__":
-    sys.argv = ["streamlit", "run", "app/streamlit_app.py"]
+    sys.argv = ["streamlit", "run", "app/db_streamlit.py"]
     sys.exit(stcli.main()) 
